f65d4b1069d5584cec89c2ebbc7cbe71eab87e06
functionxu123 <1229853312@qq.com>
2023-12-19 19:13:57 +0800

    add dot product attention

--- transformer/utils.py
+++ transformer/utils.py
@@ -1,3 +1,11 @@
-import torch
+import torch as pt
 import torch.nn as nn
+import torch.nn.functional as F
 
+def dot_product_attention(q, k, v):
+    tep = pt.matmul(q, k.t())
+    dk_sqr = pt.sqrt(k.shape[-1])
+    
+    tep = tep / dk_sqr
+    tep = F.softmax(tep)
+    return pt.matmul(tep, v)
