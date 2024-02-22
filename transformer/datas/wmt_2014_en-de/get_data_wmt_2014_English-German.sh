462efd54ad50c3d9b4995ca4221c94519f74019d
functionxu123 <1229853312@qq.com>
2024-02-22 23:45:40 +0800

    change wget args

--- transformer/datas/wmt_2014_en-de/get_data_wmt_2014_English-German.sh
+++ transformer/datas/wmt_2014_en-de/get_data_wmt_2014_English-German.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # referto : https://awslabs.github.io/sockeye/tutorials/wmt_large.html
-wget 'https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/train.en'
-wget 'https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/train.de'
+wget -nc  'https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/train.en'
+wget -nc  'https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/train.de'
 for YEAR in 2012 2013 2014; do
-    wget "https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/newstest${YEAR}.en"
-    wget "https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/newstest${YEAR}.de"
+    wget -nc  "https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/newstest${YEAR}.en"
+    wget -nc  "https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/newstest${YEAR}.de"
 done
 cat newstest{2012,2013}.en >dev.en
 cat newstest{2012,2013}.de >dev.de
@@ -13,7 +13,7 @@ cp newstest2014.en test.en
 cp newstest2014.de test.de
 
 # other datas, not necessary
-wget "https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/vocab.50K.en"
-wget "https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/vocab.50K.de"
+wget -nc  "https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/vocab.50K.en"
+wget -nc  "https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/vocab.50K.de"
 
-wget "https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/dict.en-de"
+wget -nc  "https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de/dict.en-de"
