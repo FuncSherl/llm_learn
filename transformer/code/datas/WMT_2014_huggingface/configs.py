27080bb5c4120ff176a0fc99a53e6ea04cbbe0f4
functionxu123 <1229853312@qq.com>
2024-06-16 23:32:45 +0800

    [feat]: rewrite dataset structure

---
+++ transformer/code/datas/WMT_2014_huggingface/configs.py
@@ -0,0 +1,8 @@
+import os
+import os.path as op
+
+SPECIALKEYS = ["<unk>", "<s>", "</s>", "<pad>"]
+UNKSTR = SPECIALKEYS[0]
+STARTSTR = SPECIALKEYS[1]
+ENDSTR = SPECIALKEYS[2]
+PADSTR = SPECIALKEYS[3]
---
+++ transformer/code/datas/WMT_2014_huggingface/datalodaer.py
@@ -0,0 +1,3 @@
+import os
+from .configs import *
+import datasets
